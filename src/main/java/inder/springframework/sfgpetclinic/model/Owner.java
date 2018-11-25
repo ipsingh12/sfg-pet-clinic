b7bb71485b499eb78951bc298b28e3d79d95161a
ipsingh12 <ips.genius@gmail.com>
2018-11-25 22:24:49 +0530

    Adding POJO's

---
+++ src/main/java/inder/springframework/sfgpetclinic/model/Owner.java
@@ -0,0 +1,4 @@
+package inder.springframework.sfgpetclinic.model;
+
+public class Owner extends Person {
+}
---
+++ src/main/java/inder/springframework/sfgpetclinic/model/Vet.java
@@ -0,0 +1,4 @@
+package inder.springframework.sfgpetclinic.model;
+
+public class Vet extends Person {
+}
